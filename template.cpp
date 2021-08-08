adf5405b3749c3b380d622afd01d88538e56ee79
hayashi-ryo <59258630+hayashi-ryo@users.noreply.github.com>
2021-08-08 20:47:45 +0900

    template新規作成

---
+++ template.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    // 型定義
+    int a=0;
+    int64_t b=0;
+
+    // 配列
+        // 定義
+        vector<int> s(N);
+        // 追加
+        for (int i=0;i<N;i++) {
+        cin >> s.at(i);}
+        // ソート
+        sort(s.rbegin(),s.rend());
+}
