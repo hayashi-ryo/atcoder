1210e8013eb03e517398539019aa8db81ee86b82
hayashi-ryo <59258630+hayashi-ryo@users.noreply.github.com>
2021-08-22 23:23:39 +0900

    initial

--- ABC/213/C_ReorderCards.cpp
+++ ABC/213/C_ReorderCards.cpp
@@ -3,5 +3,17 @@ using namespace std;
 
 int main() {
     // 行と列は独立しているので、座標圧縮を行うことで実現させる
-    
+    int64_t H=0,W=0,N=0;
+    cin >> H >> W >> N;
+    vector<int> a(N);
+    vector<int> b(N);
+    vector<int> vals_a(0);
+    for (int i=0;i<N;i++) {
+    cin >> a.at(i) >> b.at(i);
+    vals_a.push_back(a[i]);
+    } 
+    // vector<int> vals_b=b;
+    sort(vals_a.begin(), vals_a.end());
+    // sort(vals_b.begin(), vals_b.end());
+    vals_a.erase(unique(vals_a.begin(),vals_a.end()), vals_a.end());
 }
