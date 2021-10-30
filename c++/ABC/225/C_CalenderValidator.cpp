c280b3ca5fd52ca734020c6455ee331182545343
hayashi-ryo <59258630+hayashi-ryo@users.noreply.github.com>
2021-10-30 22:22:06 +0900

    modify

--- c++/ABC/225/C_CalenderValidator.cpp
+++ c++/ABC/225/C_CalenderValidator.cpp
@@ -12,18 +12,34 @@ int main() {
     }
     /*
     検査方針
-    1:mod12..60となること
-    2:1行下の値が+7であること    
+    1:an1+1=a+1 の等差数列であること
+    2:mod12..60となること
+    3:1行下の値が+7であること    
     */
-    //1行目のmod検査
-    int base=vec.at(0).at(0)%7;
+    //1行目の等差数列チェック
+    int diff=0;
     int flug=0;
     for (int j=1;j<M;j++) {
-        int tmpMod=vec.at(0).at(j)%7;
-        if( (tmpMod!=(base+j)) and (tmpMod!=0) ){
+        diff = vec.at(0).at(j)-vec.at(0).at(0);
+        if( diff!=j ){
             flug=1;
             break;
         }
+    }
+    //1行目のmod検査
+    int base=vec.at(0).at(0)%7;
+    for (int j=1;j<M;j++) {
+        if ((base+M)>8) {
+            flug=1;
+            break;
+        }
+        int tmpMod=vec.at(0).at(j)%7;
+        if( (tmpMod!=(base+j) ) )
+            if(not((tmpMod==0)and((vec.at(0).at(j-1)%7)==6)) ) {
+                flug=1;
+                break;
+            }
+        if(flug) {break;}
     }
     //2行目以降の検査
     for (int i=1;i<N;i++) {
