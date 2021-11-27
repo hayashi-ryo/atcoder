05414f5c3484ebe827bde005890e21740ac366f9
hayashi-ryo <59258630+hayashi-ryo@users.noreply.github.com>
2021-11-28 02:16:18 +0900

    ABC 228

--- c++/ABC/228/C_FinalDay.cpp
+++ c++/ABC/228/C_FinalDay.cpp
@@ -2,27 +2,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-    int N,k;
-    cin >> N >> k;
-    vector<int> point(N);
-    vector<int> lank(N);
-    for (int i=0;i<N;i++) {
-        int day1=0,day2=0,day3=0;
-        cin >> day1 >> day2 >> day3;
-        point.at(i)=(day1+day2+day2);
-        lank.at(i)=(day1+day2+day2);
+    long long n,k;
+    cin >> n >> k;
+    vector<int> p(n);
+    for(auto &nx : p) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        nx = a + b + c;
     }
-    sort(lank.rbegin(),lank.rend());
-    // k番目の得点を出す
-    int Threshold = lank.at(k-1);
-    // cout << "Threshold:" << Threshold << endl;
-    for (int i=0;i<N;i++) {
-        int tmpPoint = point.at(i);
-        // cout << "i:" << i << ":" << tmpPoint << endl;
-        if((tmpPoint+300)>=Threshold) {
+    vector<int> lank=p;
+    sort(lank.rbegin(), lank.rend());
+    int threshold=lank[k-1];
+
+    for (int i=0;i<n;i++) {
+        if((p[i]+300) >= threshold) {
             cout << "Yes" << endl;
         } else {
             cout << "No" << endl;
         }
-    }
+    } 
 }
