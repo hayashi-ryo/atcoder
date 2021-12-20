5be6d1cced607c50bcae4f64b05fd84cb080e926
hayashi-ryo <59258630+hayashi-ryo@users.noreply.github.com>
2021-12-21 07:46:33 +0900

    ABC232 finished

--- c++/ABC/232/A_QQsolver.cpp
+++ c++/ABC/232/A_QQsolver.cpp
@@ -2,9 +2,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
+    //コンテスト参加時に提出したコード
+    /*
     string s;
     cin >> s;
     string a=s.substr(0,1);
     string b=s.substr(2,1);
     cout << stoi(a) * stoi(b) << endl;
+    */
+    //解説みて最適化したコード
+    char a, x, b;
+    cin >> a >> x >> b;
+    cout << (int)(a-'0') * (int)(b-'0') << endl;
 }
--- c++/ABC/232/C_GraphIsomorphism.cpp
+++ c++/ABC/232/C_GraphIsomorphism.cpp
@@ -1,33 +1,38 @@
-#include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
+
 int main() {
     int n, m;
     cin >> n >> m;
-    vector<int> aoki_vec(n);
-    vector<int> takahashi_vec(n);
-    for (int i=0;i<m;i++) {
-        int tmpa=0,tmpb=0;
-        cin >> tmpa >> tmpb;
-        aoki_vec[tmpa-1]++;
-        aoki_vec[tmpb-1]++;
+    vector x(n, vector<bool>(n)), y(n, vector<bool>(n));
+    for (int i = 0; i < m; ++i) {
+        int a, b;
+        cin >> a >> b;
+        a -= 1, b -= 1;
+        x[a][b] = x[b][a] = true;
     }
-
-    for (int i=0;i<m;i++) {
-        int tmpc=0,tmpd=0;
-        cin >> tmpc >> tmpd;
-        takahashi_vec[tmpc-1]++;
-        takahashi_vec[tmpd-1]++;
+    for (int i = 0; i < m; ++i) {
+        int c, d;
+        cin >> c >> d;
+        c -= 1, d -= 1;
+        y[c][d] = y[d][c] = true;
     }
-    sort(aoki_vec.begin(),aoki_vec.end());
-    sort(takahashi_vec.begin(),takahashi_vec.end());
-    /*
-    for (int i=0;i<m;i++) {
-        cout << aoki_vec[i] << takahashi_vec[i] << endl;
-    }*/
-    if(aoki_vec==takahashi_vec) {
-        cout << "Yes" << endl;
-    } else {
-        cout << "No" << endl;
-    }
+    vector<int> p(n);
+    iota(begin(p), end(p), 0);
+    int ans = 0;
+    do {
+        bool ok = true;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (x[i][j] != y[p[i]][p[j]]) {
+                    ok = false;
+                }
+            }
+        }
+        if (ok) {
+            cout << "Yes\n";
+            return 0;
+        }
+    } while (next_permutation(begin(p), end(p)));
+    cout << "No\n";
 }
