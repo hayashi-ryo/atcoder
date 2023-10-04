162d2506886d9a0498671d7375501507e921c45b
hayashi-ryo <59258630+hayashi-ryo@users.noreply.github.com>
2023-10-04 21:02:46 +0900

    [feat]: コード改善

--- gitCommit.sh
+++ gitCommit.sh
@@ -22,8 +22,6 @@ fi
 commitMessage=$1
 echo "commitMessage:" $commitMessage
 
-# commit目的の選択
-
 echo ""
 echo "#####################"
 echo "### Select Prefix ###"
@@ -31,14 +29,18 @@ echo "#####################"
 echo ""
 
 # プレフィックスと説明文を定義
-options=("feat: 新しい機能" "fix: バグの修正" "docs: ドキュメントのみの変更" "style: 空白、フォーマット、セミコロン追加など" "refactor: 仕様に影響がないコード改善(リファクタ)" "perf: パフォーマンス向上関連" "test: テスト関連" "chore: ビルド、補助ツール、ライブラリ関連")
+options=("feat: 新しい機能" "fix: バグの修正" "docs: ドキュメントのみの変更" "style: 空白、フォーマット、セミコロン追加など" "refactor: 仕様に影響がないコード改善(リファクタ)" "perf: パフォーマンス向上関連" "test: テスト関連" "chore: ビルド、補助ツール、ライブラリ関連" "コミット中止")
 
-PS3="プレフィックスを選択してください: "
+PS3="Prefixを選択してください: "
 select option in "${options[@]}"; do
     case $REPLY in
         1|2|3|4|5|6|7|8)
             prefix=$(echo "$option" | cut -d':' -f1)
             break
+            ;;
+        9)
+            echo "コミットを中止します"
+            exit 1
             ;;
         *)
             echo "無効な選択です。1から8の数字を入力してください。"
