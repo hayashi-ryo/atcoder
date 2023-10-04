c32b26cff0324e19a8647df886e6ab88535122a4
hayashi-ryo <59258630+hayashi-ryo@users.noreply.github.com>
2023-10-04 20:51:37 +0900

    [feat]: test

---
+++ gitCommit.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+# 呼び出し方
+# gitCommit.sh "コミットメッセージ"
+
+# set -x
+
+echo ""
+echo "##################"
+echo "### git commit ###"
+echo "##################"
+echo ""
+
+if [ $# -ne 1 ]; then
+  echo "パラメータ個数ERROR"
+  # ERROR
+  exit 1
+fi
+
+# コミットメッセージの入力
+
+commitMessage=$1
+echo "commitMessage:" $commitMessage
+
+# commit目的の選択
+
+echo ""
+echo "#####################"
+echo "### Select Prefix ###"
+echo "#####################"
+echo ""
+
+# プレフィックスと説明文を定義
+options=("feat: 新しい機能" "fix: バグの修正" "docs: ドキュメントのみの変更" "style: 空白、フォーマット、セミコロン追加など" "refactor: 仕様に影響がないコード改善(リファクタ)" "perf: パフォーマンス向上関連" "test: テスト関連" "chore: ビルド、補助ツール、ライブラリ関連")
+
+PS3="プレフィックスを選択してください: "
+select option in "${options[@]}"; do
+    case $REPLY in
+        1|2|3|4|5|6|7|8)
+            prefix=$(echo "$option" | cut -d':' -f1)
+            break
+            ;;
+        *)
+            echo "無効な選択です。1から8の数字を入力してください。"
+            ;;
+    esac
+done
+
+
+commit_message="[$prefix]: $1"
+
+git add .
+git commit -m "$commit_message"
+
+echo ""
+echo "#######################"
+echo "### commit complete ###"
+echo "#######################"
+echo ""
