7122b0a431fd6d6e7302fb88fadb773434313bab
hayashi-ryo <59258630+hayashi-ryo@users.noreply.github.com>
2024-02-29 15:27:35 +0900

    [feat]: Complete to ABC issue of AB335

---
+++ docker/problems/cpp/ABC/abc335/a/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+#define rep(i, x) for (int i = 0; i < (x); i++)
+
+int main()
+{
+  string S;
+  cin >> S;
+  S.pop_back();
+  S += '4';
+  cout << S << endl;
+  return 0;
+}
