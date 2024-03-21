d4140a4b9bf3b7a78b484d8651f8ff4468862c7e
hayashi-ryo <59258630+hayashi-ryo@users.noreply.github.com>
2024-03-21 19:29:20 +0900

    [feat]: 4章完了

--- docker/problems/cpp/tessoku-book/a16/main.cpp
+++ docker/problems/cpp/tessoku-book/a16/main.cpp
@@ -6,4 +6,28 @@ using ll = long long;
 
 int main()
 {
+  int N;
+  cin >> N;
+  vector<int> a(N);
+  vector<int> b(N);
+  vector<int> dp(N);
+  for (int i = 1; i < N; i++)
+  {
+    cin >> a[i];
+  }
+  for (int i = 2; i < N; i++)
+  {
+    cin >> b[i];
+  }
+
+  // 配列dpの計算
+  dp[0] = 0;
+  dp[1] = a[1];
+  for (int i = 2; i < N; i++)
+  {
+    dp[i] = min(dp[i - 1] + a[i], dp[i - 2] + b[i]);
+  }
+  cout << dp[N - 1] << endl;
+
+  return 0;
 }
--- docker/problems/cpp/tessoku-book/a19/main.cpp
+++ docker/problems/cpp/tessoku-book/a19/main.cpp
@@ -6,4 +6,45 @@ using ll = long long;
 
 int main()
 {
+  ll N, W;
+  cin >> N >> W;
+  ll w[109], v[109];
+  ll dp[109][100009];
+  for (int i = 1; i <= N; i++)
+  {
+    cin >> w[i] >> v[i];
+  }
+
+  for (int i = 1; i <= N; i++)
+  {
+    for (int j = 0; j <= W; j++)
+    {
+      dp[i][j] = -1'000'000'000'000LL;
+    }
+  }
+
+  dp[0][0] = 0;
+  for (int i = 1; i <= N; i++)
+  {
+    for (int j = 0; j <= W; j++)
+    {
+      if (j < w[i])
+      {
+        dp[i][j] = dp[i - 1][j];
+      }
+      else
+      {
+        dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]);
+      }
+    }
+  }
+
+  ll ans = 0;
+  for (int i = 0; i <= W; i++)
+  {
+    ans = max(ans, dp[N][i]);
+  }
+  cout << ans << endl;
+
+  return 0;
 }
