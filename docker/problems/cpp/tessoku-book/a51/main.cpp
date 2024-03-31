6630b146d04b78b9670ab10734b271b28e3dbcfd
hayashi-ryo <59258630+hayashi-ryo@users.noreply.github.com>
2024-03-31 22:28:14 +0900

    [feat]: 8章完了

--- docker/problems/cpp/tessoku-book/a51/main.cpp
+++ docker/problems/cpp/tessoku-book/a51/main.cpp
@@ -6,4 +6,28 @@ using ll = long long;
 
 int main()
 {
+  int Q;
+  cin >> Q;
+  stack<string> books;
+  for (int i = 0; i < Q; i++)
+  {
+    int q;
+    string bookName;
+    cin >> q;
+    if (q == 1)
+    {
+      cin >> bookName;
+      books.push(bookName);
+    }
+    else if (q == 2)
+    {
+      cout << books.top() << endl;
+    }
+    else if (q == 3)
+    {
+      books.pop();
+    }
+  }
+
+  return 0;
 }
--- docker/problems/cpp/tessoku-book/a54/main.cpp
+++ docker/problems/cpp/tessoku-book/a54/main.cpp
@@ -6,4 +6,24 @@ using ll = long long;
 
 int main()
 {
+  int Q;
+  map<string, int> results;
+  cin >> Q;
+  for (int i = 0; i < Q; i++)
+  {
+    int q, point;
+    string studentName;
+    cin >> q >> studentName;
+    if (q == 1)
+    {
+      cin >> point;
+      results[studentName] = point;
+    }
+    else if (q == 2)
+    {
+      cout << results[studentName] << endl;
+    }
+  }
+
+  return 0;
 }
