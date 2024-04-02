b940619ec5c515031f165bd24d85cf33874051bd
hayashi-ryo <59258630+hayashi-ryo@users.noreply.github.com>
2024-04-02 22:07:08 +0900

    [feat]: 9章完了

--- docker/problems/cpp/tessoku-book/a62/main.cpp
+++ docker/problems/cpp/tessoku-book/a62/main.cpp
@@ -4,6 +4,47 @@ using ll = long long;
 
 #define rep(i, x) for (int i = 0; i < (x); i++)
 
+int N, M;
+vector<bool> visited(100009);
+vector<int> graph[100009];
+
+void dfs(int pos)
+{
+  visited[pos] = true;
+  for (int i = 0; i < graph[pos].size(); i++)
+  {
+    int nex = graph[pos][i];
+    if (visited[nex] == false)
+    {
+      dfs(nex);
+    }
+  }
+  return;
+}
+
 int main()
 {
+  cin >> N >> M;
+
+  for (int i = 0; i < M; i++)
+  {
+    int a, b;
+    cin >> a >> b;
+    graph[a - 1].push_back(b - 1);
+    graph[b - 1].push_back(a - 1);
+  }
+
+  rep(i, N) visited[i] = false;
+  dfs(0);
+  string ans = "The graph is connected.";
+  for (int i = 0; i < N; i++)
+  {
+    if (visited[i] == false)
+    {
+      ans = "The graph is not connected.";
+    }
+  }
+
+  cout << ans << endl;
+  return 0;
 }
--- docker/problems/cpp/tessoku-book/a63/main.cpp
+++ docker/problems/cpp/tessoku-book/a63/main.cpp
@@ -6,4 +6,41 @@ using ll = long long;
 
 int main()
 {
+  // 入力
+  int N, M;
+  cin >> N >> M;
+  vector<int> graph[N];
+  vector<int> dist(N);
+  queue<int> Q;
+
+  for (int i = 0; i < M; i++)
+  {
+    int a, b;
+    cin >> a >> b;
+    graph[a - 1].push_back(b - 1);
+    graph[b - 1].push_back(a - 1);
+  }
+
+  // 幅優先探索
+  rep(i, N) dist[i] = -1;
+  Q.push(0);
+  dist[0] = 0;
+  while (!Q.empty())
+  {
+    int pos = Q.front();
+    Q.pop();
+    for (int i = 0; i < graph[pos].size(); i++)
+    {
+      int to = graph[pos][i];
+      if (dist[to] == -1)
+      {
+        dist[to] = dist[pos] + 1;
+        Q.push(to);
+      }
+    }
+  }
+
+  // 出力
+  rep(i, N) cout << dist[i] << endl;
+  return 0;
 }
