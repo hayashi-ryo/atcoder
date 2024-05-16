5a97fc4299db460713c642988f693342e0497551
hayashi-ryo <59258630+hayashi-ryo@users.noreply.github.com>
2024-05-17 03:59:44 +0900

    [feat]: 2章完了

--- docker/problems/cpp/tessoku-book/a08/main.cpp
+++ docker/problems/cpp/tessoku-book/a08/main.cpp
@@ -6,5 +6,49 @@ using ll = long long;
 
 int main()
 {
+  int H, W, Q;
+  cin >> H >> W;
+  vector<vector<int>> cell(H, vector<int>(W, 0));
+  for (int i = 0; i < H; i++)
+  {
+    for (int j = 0; j < W; j++)
+    {
+      cin >> cell[i][j];
+    }
+  }
+  cin >> Q;
+  vector<int> A(Q), B(Q), C(Q), D(Q);
+  for (int i = 0; i < Q; i++)
+  {
+    cin >> A[i] >> B[i] >> C[i] >> D[i];
+    A[i]--;
+    B[i]--;
+    C[i]--;
+    D[i]--;
+  }
+
+  // 二次元累積和の計算
+  vector<vector<int>> sumCell(H + 1, vector<int>(W + 1, 0));
+  for (int i = 1; i <= H; i++)
+  {
+    for (int j = 1; j <= W; j++)
+    {
+      sumCell[i][j] = cell[i - 1][j - 1] + sumCell[i - 1][j] + sumCell[i][j - 1] - sumCell[i - 1][j - 1];
+    }
+  }
+
+  // クエリに応じた範囲内の合計値を計算
+  for (int i = 0; i < Q; i++)
+  {
+    int ans = sumCell[C[i] + 1][D[i] + 1];
+    if (A[i] > 0)
+      ans -= sumCell[A[i]][D[i] + 1];
+    if (B[i] > 0)
+      ans -= sumCell[C[i] + 1][B[i]];
+    if (A[i] > 0 && B[i] > 0)
+      ans += sumCell[A[i]][B[i]];
+    cout << ans << endl;
+  }
+
   return 0;
 }
--- docker/problems/cpp/tessoku-book/a10/main.cpp
+++ docker/problems/cpp/tessoku-book/a10/main.cpp
@@ -6,5 +6,62 @@ using ll = long long;
 
 int main()
 {
+  int N, D;
+  cin >> N;
+  vector<int> A(N);
+  for (int i = 0; i < N; i++)
+  {
+    cin >> A[i];
+  }
+  cin >> D;
+  vector<int> L(D), R(D);
+  for (int i = 0; i < D; i++)
+  {
+    cin >> L[i] >> R[i];
+    L[i]--;
+    R[i]--;
+  }
+
+  // 左右の端から最も大きい部屋の情報を記録していって、L-Rの範囲外で最大の部屋を決定する。
+  vector<int> leftMaxRoom(N, 0);
+  vector<int> rightMaxRoom(N, 0);
+
+  // 左から最大値を記録
+  for (int i = 0; i < N; i++)
+  {
+    if (i == 0)
+    {
+      leftMaxRoom[i] = A[i];
+    }
+    else
+    {
+      leftMaxRoom[i] = max(leftMaxRoom[i - 1], A[i]);
+    }
+  }
+
+  // 右から最大値を記録
+  for (int i = N - 1; i >= 0; i--)
+  {
+    if (i == N - 1)
+    {
+      rightMaxRoom[i] = A[i];
+    }
+    else
+    {
+      rightMaxRoom[i] = max(rightMaxRoom[i + 1], A[i]);
+    }
+  }
+
+  // 出力
+  for (int i = 0; i < D; i++)
+  {
+    int maxOutside = 0;
+    if (L[i] > 0)
+      maxOutside = max(maxOutside, leftMaxRoom[L[i] - 1]);
+    if (R[i] + 1 < N)
+      maxOutside = max(maxOutside, rightMaxRoom[R[i] + 1]);
+    cout << maxOutside << endl;
+  }
+
   return 0;
 }
